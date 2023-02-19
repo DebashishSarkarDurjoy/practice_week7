462e15dc49e9262995658eee8758f0ffe20c9eb6
DebashishSarkarDurjoy <dsdjoy4@gmail.com>
2023-02-19 20:15:56 +1100

    nk ladders top down dynamic

--- nkLadders.cpp
+++ nkLadders.cpp
@@ -2,22 +2,25 @@
 
 using namespace std;
 
-int countWays(int n, int k) {
+int countWays(int n, int k, int* dp) {
     if (n == 0) return 1;
     if (n < 0) return 0;
+    if (dp[n] != 0) return dp[n];
 
     int ans = 0;
     for (int jump = 1; jump <= k; jump++) {
-        ans += countWays(n-jump, k);
+        ans += countWays(n-jump, k, dp);
     }
-
+    dp[n] = ans;
     return ans;
 }
 
 int main(void) {
     int n, k;
     cin >> n >> k;
-    cout << countWays(n, k) << endl;
+
+    int dp[100] = {0};
+    cout << countWays(n, k, dp) << endl;
 
     return 0;
 }
